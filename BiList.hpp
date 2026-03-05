e5d53896a0d61a48905f033573149b2b181e2584
Stave143 <staveplay@mail.ru>
2026-03-05 20:39:59 +0300

    change BiList.hpp

--- BiList.hpp
+++ BiList.hpp
@@ -7,20 +7,16 @@ namespace permyakov {
     BiList * next;
     BiList * prev;
   };
-  template < class T > T value(BiList < T > * list);
-  template < class T > BiList < T > * next(BiList < T > * list);
-  template < class T > BiList < T > * prev(BiList < T > * list);
-  template < class T > void setNext(BiList < T > * list, BiList < T > * to);
-  template < class T > void setPrev(BiList < T > * list, BiList < T > * to);
+  template < class T > T value(const BiList < T > * list);  
+  template < class T > void setValue(BiList < T > * list, const T v);
+  template < class T > BiList < T > * next(const BiList < T > * list);
+  template < class T > BiList < T > * prev(const BiList < T > * list);
+  template < class T > void setNext(BiList < T > * list, const BiList < T > * to);
+  template < class T > void setPrev(BiList < T > * list, const BiList < T > * to);
   template < class T > BiList < T > * first(BiList < T > * list);
   template < class T > BiList < T > * last(BiList < T > * list);
   template < class T > BiList < T > * addAfter(BiList < T > * list1, BiList < T > * list2);
-  template < class T > void clearToEnd(BiList < T > * list, BiList < T > * end);
+  template < class T > BiList < T > * clearToEnd(BiList < T > * list, BiList < T > * end);
+  template < class T > BiList < T > * convertVecToList(T * vec, const size_t size);
 }
 #endif
-/* 1
-BiList(); конструктор
-BiList(BiList); копирование
-BiList(BiList&&); присваивание
-~T(); деструктор
-*/
