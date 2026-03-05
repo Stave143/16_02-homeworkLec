2535ca92dce9c25233b10046faa73857f01385b7
Stave143 <staveplay@mail.ru>
2026-03-05 20:47:09 +0300

    add convertVecToList to BiList.cpp

--- BiList.cpp
+++ BiList.cpp
@@ -2,31 +2,37 @@
 
 namespace permyakov {
   template < class T > 
-  T value(BiList < T > * list)
+  T value(const BiList < T > * list)
   {
     return list -> val;
   }
 
   template < class T > 
-  BiList < T > * next(BiList < T > * list)
+  void setValue(BiList < T > * list, T v)
+  {
+    list -> val = v;
+  }
+
+  template < class T > 
+  BiList < T > * next(const BiList < T > * list)
   {
     return list -> next;
   }
 
   template < class T > 
-  BiList < T > * prev(BiList < T > * list)
+  BiList < T > * prev(const BiList < T > * list)
   {
     return list -> prev;
   }
 
   template < class T > 
-  void setNext(BiList < T > * list, BiList < T > * to)
+  void setNext(BiList < T > * list, const BiList < T > * to)
   {
     list -> next = to;
   }
 
   template < class T > 
-  void setPrev(BiList < T > * list, BiList < T > * to)
+  void setPrev(BiList < T > * list, const BiList < T > * to)
   {
     list -> prev = to;
   }
@@ -59,7 +65,7 @@ namespace permyakov {
   }
 
   template < class T > 
-  void clearTo(BiList < T > * list, BiList < T > * end)
+  BiList < T > * clearTo(BiList < T > * list, BiList < T > * end)
   {
     BiList < T > * temp = nullptr;
     if(prev(list)) {
@@ -76,5 +82,27 @@ namespace permyakov {
     if(end) {
       setPrev(end, list);
     }
+    return end;
+  }
+
+  template < class T > 
+  BiList < T > * convertVecToList(const T * vec, const size_t size)
+  {
+    BiList < int > * list = nullptr;
+    list = new BiList < int >;//конструктор по умолчанию
+    BiList < int > * head = list;
+    for(size_t i = 0; i < size; ++i){
+      BiList < int > * temp = list;
+      setValue(list, vec[i]);
+      if(i + 1 < size) {
+        try {
+          list = new BiList < int > {0, nullptr, temp};//конструктор присваивания
+        } catch (...) {
+          clearTo(head, nullptr);
+          throw;
+        }
+      }
+    }
+    return head;
   }
 }
